2b22a49bd80600691796742ff6894bdbb73c2159
AlixOrf <alix.orfeuvre@epitech.digital>
2025-10-13 17:01:20 +0200

    Strapi mis en place

--- Lilo/src/api/mood/controllers/mood.js
+++ Lilo/src/api/mood/controllers/mood.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  article controller
+ * mood controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController('api::mood.mood');
